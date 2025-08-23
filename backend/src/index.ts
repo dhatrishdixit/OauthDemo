a44859c6066f63e1b4f3130a074a0e2be62cbc60
Dhatish Singh Dixit <dhatrish29@gmail.com>
2025-08-23 12:36:16 +0530

    ran first test on successfull backend connection

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+async function test(){
+    try {
+        const res = await prisma.user.create({
+        data:{
+            name:"test2",
+            email:"test3"
+        }
+    })
+
+    console.log("test successfull : ",res)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+test();
+
