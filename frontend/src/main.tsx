c24e441ea93963ebf42f5cbeaa846b2fa3f6d5d4
Dhatish Singh Dixit <dhatrish29@gmail.com>
2025-09-08 17:21:53 +0530

    combined gitignore

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './components/theme-provider.tsx'
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
